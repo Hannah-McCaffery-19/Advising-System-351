aea359419a6afc1b43338a730f585e36bc62044f
Hannah-McCaffery-19 <hannah.mccaffery.19@cnu.edu>
2022-04-13 16:51:34 -0400

    1.6 reworking DB, slight login modifications; DB changes not yet reflected in sql file

--- auth.php
+++ auth.php
@@ -15,14 +15,14 @@ if (mysqli_connect_errno()) {
 }
 
 //Student ID and Password not entered throw
-if (!isset($_POST['student_ID'], $_POST['password'])) {
+if (!isset($_POST['student ID'], $_POST['password'])) {
 	exit('Please enter your CNU ID and password.');
 }
 
 // Prepare SQL - uncertain if this is the proper way to do this, ask lapke
-if ($stmt = $con->prepare('SELECT password FROM student WHERE student_ID = ?')) {
+if ($stmt = $con->prepare('SELECT password FROM student WHERE student ID = ?')) {
 	// Bind student student ID to string
-	$stmt->bind_param('s', $_POST['student_ID']);
+	$stmt->bind_param('s', $_POST['student ID']);
 	$stmt->execute();
 	// Store the result, check database
 	$stmt->store_result();
@@ -35,7 +35,7 @@ if ($stmt = $con->prepare('SELECT password FROM student WHERE student_ID = ?')) 
 	if ($_POST['password'] === $password) {
 		// Verification success, create user session
 		session_regenerate_id();
-		$_SESSION['user_id'] = $_POST['student_ID'];
+		$_SESSION['user_id'] = $_POST['student ID'];
 		echo 'Welcome, student ' . $_SESSION['user_id'];
 	} else {
 		// Incorrect password

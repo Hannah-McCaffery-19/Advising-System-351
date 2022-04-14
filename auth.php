e2a872729927de37ad1e035d8022c1793c7feb58
Hannah-McCaffery-19 <hannah.mccaffery.19@cnu.edu>
2022-04-14 01:57:08 -0400

    1.7 the login form in index is once again communicating with auth.php, session storage and redirect to homepage not yet implemented

--- auth.php
+++ auth.php
@@ -14,39 +14,12 @@ if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-//Student ID and Password not entered throw
-if (!isset($_POST['student ID'], $_POST['password'])) {
-	exit('Please enter your CNU ID and password.');
+if (isset($_POST['username']) and isset($_POST['password'])){
+	echo 'ID and password received';
 }
 
-// Prepare SQL - uncertain if this is the proper way to do this, ask lapke
-if ($stmt = $con->prepare('SELECT password FROM student WHERE student ID = ?')) {
-	// Bind student student ID to string
-	$stmt->bind_param('s', $_POST['student ID']);
-	$stmt->execute();
-	// Store the result, check database
-	$stmt->store_result();
-	
-	
-	if ($stmt->num_rows > 0) {
-	$stmt->bind_result($password);
-	$stmt->fetch();
-	// Account exists, verify password
-	if ($_POST['password'] === $password) {
-		// Verification success, create user session
-		session_regenerate_id();
-		$_SESSION['user_id'] = $_POST['student ID'];
-		echo 'Welcome, student ' . $_SESSION['user_id'];
-	} else {
-		// Incorrect password
-		echo 'Incorrect School ID and/or password!';
-	}
-} else {
-	// Incorrect student_ID
-	echo 'Incorrect School ID and/or password!';
+else {
+	echo 'ID and Password were not set';
 }
 
-
-	$stmt->close();
-}
 ?>
